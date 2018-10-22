a57d46b69f18379ba1b213579145b32089babc2d
Burak Inan <burakinan1@hotmail.com>
2018-10-22 16:42:49 +0200

    as1 fix

--- assignment-1/src/main/java/nl/hva/ict/ds/LinkedList.java
+++ assignment-1/src/main/java/nl/hva/ict/ds/LinkedList.java
@@ -1,5 +1,7 @@
 package nl.hva.ict.ds;
 
+import java.util.NoSuchElementException;
+
 /**
  * A linked list that adds elements to the end of the list and that retrieves elements from the end of the list as fast
  * as from the head of the list.
@@ -25,9 +27,16 @@ public class LinkedList<T> {
         }
     }
 
-    private Node head;
-    private Node tail;
-    private int size = 0;
+    private Node head; // De eerste Node
+    private Node tail; // De laatste Node
+    private int size = 0; // De grootte van de lijst, word geïncrement bij elke add/remove
+
+    /**
+     * @return head, die null is wanneer de lijst leeg is.
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
 
     /**
      * Adds a new element to the end of this list. The performance of this method is guaranteed to be constant, in other
@@ -37,18 +46,13 @@ public class LinkedList<T> {
      * @param element the element that is added to the list.
      */
     public void add(T element) {
-
-        Node n = new Node(element, null);
-
+        Node node = new Node(element, null);
         if (head == null) {
-            head = n;
+            head = node;
+            tail = node;
         } else {
-            Node n2 = head;
-
-            while (n2.next != null) {
-                n2 = n2.next;
-            }
-            n2.next = n;
+            tail.next = node;
+            tail = node;
         }
         size++;
     }
@@ -56,6 +60,7 @@ public class LinkedList<T> {
     /**
      * Returns an element from the list. If the index is negative or the element does not exists
      * an IllegalArgumentException is throw containing the reason in the message.
+     *
      * @param index the index, counted from the first element, of the element that must be returned.
      */
     public T get(int index) {
@@ -63,26 +68,23 @@ public class LinkedList<T> {
             throw new IllegalArgumentException("Negative index is not allowed.");
         }
 
+        if (head == null) throw new NoSuchElementException("List is empty.");
+
         Node current = head;
-        for (int i = 0; i < index;i++) {
+        for (int i = 0; i < index; i++) {
             if (current.next == null) {
                 throw new IndexOutOfBoundsException("Reached end of list.");
             }
             current = current.next;
         }
-
         return current.value;
     }
 
+    /**
+     * @return tail, het laatste element in de lijst.
+     */
     public T getLast() {
-        Node current = head;
-        for (int i = 0; i < size(); i++) {
-            if (current.next == null) {
-                tail = current;
-            } else {
-                current = current.next;
-            }
-        }
+        if (head == null) throw new NoSuchElementException("List is empty.");
         return tail.value;
     }
 
@@ -96,31 +98,63 @@ public class LinkedList<T> {
         recursiveDelete(element, head);
     }
 
+    /**
+     * Recursieve delete, we gaan net zo lang door totdat we aan het einde van de lijst
+     * aan zijn gekomen. Dit moet zo gedaan worden omdat een element vaker voor kan komen in de lijst.
+     *
+     * @param target,  het element dat wordt verwijderd.
+     * @param current, Node die wordt meegegeven om door de lijst heen te stappen.
+     */
     private void recursiveDelete(T target, Node current) {
-        Node prev = current;
-        if (current == null) {
-            size--;
+        Node temp = current;
+        tail = current;
+
+        if (head == null) {
             return;
         }
+
         if (current.value == target) {
-            prev = current.next;
+            if (head.next == null) {
+                head = null;
+            } else {
+                head = current.next;
+                current = current.next;
+            }
+            size--;
+            recursiveDelete(target, current);
+        }
+
+        if (current.next == null) {
+            return;
+        }
+
+        if (current.next.value == target) {
+            current = current.next;
+            temp.next = current.next;
             size--;
         }
+
         if (current.next != null) {
-            recursiveDelete(target, prev.next);
-        } else {
-            return;
+            recursiveDelete(target, current.next);
         }
     }
 
+    /**
+     * Visualiseert hoe de LinkedList er uit komt te zien.
+     * Voorbeeld: Node1 --> Node2 --> null
+     */
     public void print() {
         Node node = head;
 
-        while (node.next != null) {
-            System.out.println(node.value);
-            node = node.next;
+        if (head == null) {
+            System.out.println("null");
+        } else {
+            while (null != node) {
+                System.out.print(node.value + " --> ");
+                node = node.next;
+            }
+            System.out.println("null");
         }
-        System.out.println(node.value);
     }
 
     /**
--- assignment-1/src/test/java/nl/hva/ict/ds/LinkedListTest.java
+++ assignment-1/src/test/java/nl/hva/ict/ds/LinkedListTest.java
@@ -3,6 +3,8 @@ package nl.hva.ict.ds;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
@@ -103,4 +105,72 @@ public class LinkedListTest {
         testList.add("one");
         assertEquals(1, testList.size());
     }
+
+
+    /* EXTRA UNIT TESTS */
+
+    /**
+     * Deze methode checkt na het verwijderen van twee verschillende elementen, de grootte van de lijst.
+     */
+    @Test
+    public void threeElementsInList() {
+        testList.add("one");
+        testList.add("one");
+        testList.add("two");
+        testList.add("three");
+        testList.add("four");
+        testList.add("five");
+        testList.delete("one");
+        testList.delete("three");
+
+        assertEquals(3, testList.size());
+    }
+
+    /**
+     * Deze methode test de isEmpty() methode, die true of false returnt wanneer de lijst leeg is.
+     */
+    @Test
+    public void isEmpty() {
+        testList.add("one");
+        testList.add("one");
+        testList.add("one");
+        testList.add("one");
+        testList.delete("one");
+        assertEquals(true, testList.isEmpty());
+    }
+
+    /**
+     * Deze methode checkt het laatste element na het verwijderen van elementen.
+     */
+    @Test
+    public void getLastAfterDelete() {
+        testList.add("one");
+        testList.add("two");
+        testList.add("one");
+        testList.add("three");
+        testList.delete("three");
+        assertEquals("one", testList.getLast());
+    }
+
+    /**
+     * Deze methode test of er een exception wordt meegegeven wanneer je zoekt naar een element in een lege lijst.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void getFromEmptyList() {
+        testList.get(2);
+    }
+
+    /**
+     * Deze methode test het zoeken van bepaalde elementen op een index, na het verwijderen van andere elementen.
+     */
+    @Test
+    public void getAt() {
+        testList.add("one");
+        testList.add("two");
+        testList.add("one");
+        testList.add("three");
+        testList.delete("one");
+        assertEquals("two", testList.get(0));
+    }
+
 }
